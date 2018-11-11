51d71341aa6dd76013f265c68a1e477dbda9ccc2
raferguson <rferguson@laughlin.com>
2018-11-11 16:42:12 -0600

    Basic init, config file ignore, testing framework in env

---
+++ project/__init__.py
@@ -0,0 +1,13 @@
+#################
+#### imports ####
+#################
+ 
+from flask import Flask
+ 
+ 
+################
+#### config ####
+################
+ 
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('flask.cfg')
