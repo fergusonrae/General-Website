4df9543ff85995159abd8ca2cc2ed7e5189e5b40
raferguson <rferguson@laughlin.com>
2018-11-23 16:21:51 -0600

    Refactor, add html base, run page
    Creating website skeleton

--- project/models/views.py
+++ project/models/views.py
@@ -15,7 +15,11 @@ models_blueprint = Blueprint('models', __name__, template_folder='templates')
 ################
 #### routes ####
 ################
- 
-@models_blueprint.route('/beauty')
+
+@models_blueprint.route('/models')
+def models_homepage():
+    return render_template('models_homepage.html')
+
+@models_blueprint.route('/models/beauty')
 def beauty_classification():
     return render_template('beauty_classification.html')
